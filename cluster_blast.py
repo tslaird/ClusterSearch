8b2b1c0997e6cfd3bd975d2b6eb13689f871609a
tslaird <tslaird@ucdavis.edu>
2023-05-01 13:49:10 -0700

    redid functions for parsing blast outputs

--- cluster_blast.py
+++ cluster_blast.py
@@ -2,7 +2,6 @@ import psutil
 import os
 import subprocess
 
-print("Initiating blastp search")
 #blast the db using blastp
 
 
@@ -14,5 +13,5 @@ def blastout2blasttable(input_file):
 def cluster_blast(query_file,database_file, eval=0.1, n_cpus=1, results_directory='results'):
     if not os.path.exists("results/"):
         os.mkdir("results/")
-    subprocess.call(['blastp','-db',database_file,'-query',query_file,'-out',results_directory+"/"+'cluster_blast_out.txt','-outfmt','6 qseqid qgi qacc sseqid sallseqid sgi sallgi sacc sallacc qstart qend sstart send qseq sseq evalue bitscore score length qlen slen pident nident mismatch positive gapopen gaps ppos frames qframe sframe btop stitle salltitles sstrand qcovs qcovhsp qcovus','-max_target_seqs','50000','-num_threads',str(number_of_cpus),'-evalue',string(eval)])
+    subprocess.call(['blastp','-db',database_file,'-query',query_file,'-out',results_directory+"/"+'cluster_blast_out.txt','-outfmt','6 qseqid qgi qacc sseqid sallseqid sgi sallgi sacc sallacc qstart qend sstart send qseq sseq evalue bitscore score length qlen slen pident nident mismatch positive gapopen gaps ppos frames qframe sframe btop stitle salltitles sstrand qcovs qcovhsp qcovus','-max_target_seqs','50000','-num_threads',str(n_cpus),'-evalue',str(eval)])
     blastout2blasttable(input_file=results_directory+"/"+'cluster_blast_out.txt',)
--- make_blastdb.py
+++ make_blastdb.py
@@ -30,11 +30,9 @@ def make_blastdb(file_list_index):
     subprocess.call(['makeblastdb','-in',combined_file,'-dbtype','prot'])
 
 def make_blastdb_parallel(input_dir="fasta_files_combined", n_cpus=1):
-    combined_files_list = glob.glob(input_dir+"/combo_*")
-    print(combined_files_list)
+    combined_files_list = glob.glob(input_dir+"/combo_*.phr")
     with concurrent.futures.ProcessPoolExecutor(max_workers=n_cpus) as executor:
         for _ in executor.map(make_blastdb, range(len(combined_files_list))):
             pass
     combined_files_str = " ".join(combined_files_list)
-    print(combined_files_str)
     subprocess.call(['blastdb_aliastool','-dblist',combined_files_str,'-dbtype','prot','-out',input_dir+'/fasta_files_combined_master','-title','all_proteins_combined_master'])
